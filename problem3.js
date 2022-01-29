c5ab079375fe7de045bf9be61319302948c20088
Piyush5087 <63440276+Piyush5087@users.noreply.github.com>
2022-01-29 17:57:07 +0530

    Add files via upload

---
+++ problem3.js
@@ -0,0 +1,14 @@
+//Javascript program to swap two numbers
+
+var a= 20;
+var b= 30;
+console.log("Value of a and b are respectively: "+ a +" and "+ b);
+var temp;
+
+temp=a;
+a=b;
+b=temp;
+
+console.log("New value of a and are respectively: "+ a +" and "+ b);
+
+
