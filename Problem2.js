9a8bc93bc033166f73ff7e33ee2cbddd865304c5
Piyush5087 <63440276+Piyush5087@users.noreply.github.com>
2022-01-29 17:48:34 +0530

    Add files via upload

---
+++ Problem2.js
@@ -0,0 +1,8 @@
+//Javascript program to calculate area of a triangle
+
+const base= 30;    //Value in cm(centimeter)
+const height= 40;   //Value in cm
+
+const areaOfTriangle=(base*height)/2;
+
+console.log(areaOfTriangle+ " cm^2");
