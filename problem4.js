c3df51d57cc402cee9b377ac992c88c06a617b84
Piyush5087 <63440276+Piyush5087@users.noreply.github.com>
2022-01-29 18:07:21 +0530

    Add files via upload

---
+++ problem4.js
@@ -0,0 +1,17 @@
+//Javascript program to check if a number is positive negative or zero
+
+// const number= 10;
+// const number= 0;
+const number= -10;
+
+if(number>0){
+  console.log("The number is positive");
+}
+
+else if(number==0){
+  console.log("The given number is zero");
+}
+else{
+  console.log("The number is negative")
+}
+
