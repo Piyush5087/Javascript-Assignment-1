d83a41abb92190328e5aba8843ab082daead4a94
Piyush5087 <63440276+Piyush5087@users.noreply.github.com>
2022-01-29 18:16:53 +0530

    Add files via upload

---
+++ problem5.js
@@ -0,0 +1,9 @@
+//Javascript program to check if a number is odd or even
+
+
+const number= 10;
+// const number= 7;
+
+const result= (number % 2 == 0)? "even" : "odd";
+
+console.log(`The number is ${result}`);
